d3e672a597ad98c78c54c6aac2ff2778f57f44e3
cristianmeelo <cristianmeelo@gmail.com>
2023-03-31 16:49:20 -0300

    :sparkles: feat: finalized app

--- react-redux-ecommerce/src/components/product/component/Index.tsx
+++ react-redux-ecommerce/src/components/product/component/Index.tsx
@@ -1,22 +1,34 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { IProduct } from "../../../types/types";
+
 
 export const ProductComponent = () => {
-    const products = useSelector((state: any) => state.allProducts.products);
-    const { id, title } = products[0];
+  const products = useSelector((state: any) => state.allProducts.products);
 
+  const renderList = products.map((product: IProduct) => {
     return (
-        <div className="four column wide">
-            <div className="ui link cards">
-                <div className="card">
-                    <div className="image"></div>
-                    <div className="content">
-                        <div className="header">
-                            {title}
-                        </div>
-                    </div>
-                </div>
+      <div className="four wide column" key={product.id}>
+        <Link to={`/product/${product.id}`} >
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={product.image} alt={product.title} />
+              </div>
+              <div className="content">
+                <div className="header">{product.title}</div>
+                <div className="meta price">$ {product.price}</div>
+                <div className="meta">{product.category}</div>
+              </div>
             </div>
-
-        </div>
+          </div>
+        </Link>
+      </div>
     )
+  });
+  return (
+    <>
+      {renderList}
+    </>
+  )
 }
--- react-redux-ecommerce/src/components/product/details/Index.tsx
+++ react-redux-ecommerce/src/components/product/details/Index.tsx
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchProductDetail } from '../../../services/api';
+import { selectedProduct, removeSelectedProduct } from '../../../redux/products/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { IProduct } from '../../../types/types';
 
 export const ProductDetails = () => {
-    return (
-        <div>
-            <h1>detais</h1>
+  const product: IProduct = useSelector((state: any) => state.product);
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+
+  const handleFetchProductDetail: VoidFunction = async () => {
+    try {
+      const response = await fetchProductDetail(productId);
+      dispatch(selectedProduct(response.data))
+    }
+    catch (error: any) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (productId && productId !== "") handleFetchProductDetail();
+    return () => {
+      dispatch(removeSelectedProduct());
+    }
+  }, [productId])
+
+
+  return (
+    <div className="ui grid container">
+      {Object.keys(product).length === 0 ? (<>... Loading</>) :
+        <div className="ui placeholder segment">
+          <div className="ui two column stackable center aligned grid">
+            <div className="ui vertical divider">💎</div>
+            <div className="middle aligned row">
+              <div className="column lp">
+                <img className="ui fluid image" src={product.image} />
+              </div>
+              <div className="column rp">
+                <h1>{product.title}</h1>
+                <h2>
+                  <a className="ui teal tag label">${product.price}</a>
+                </h2>
+                <h3 className="ui brown block header">{product.category}</h3>
+                <p>{product.description}</p>
+                <div className="ui vertical animated button" tabIndex={0}>
+                  <div className="hidden content">
+                    <i className="shop icon"></i>
+                  </div>
+                  <div className="visible content">Add to Cart</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      }
+    </div>
+  )
 }
+
 
