e965e66bc68dc5363c59ddde14c2e31b9e60d380
cristianmeelo <cristianmeelo@gmail.com>
2023-03-31 12:01:14 -0300

    :sparkles: feat: created reducers

---
+++ react-redux-ecommerce/redux/reducers/index.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from "@reduxjs/toolkit"
+import { productReducer } from "../products/reducer"
+
+export const reducers = combineReducers({
+    allProducts: productReducer,
+})
