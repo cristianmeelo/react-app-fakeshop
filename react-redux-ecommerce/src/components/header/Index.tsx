efbd1015ad6c2fd7403a68e94401dd18fc8ae4b1
cristianmeelo <cristianmeelo@gmail.com>
2023-03-31 14:55:09 -0300

    ⚛️ : feat: create header component

---
+++ react-redux-ecommerce/src/components/header/Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header className='ui fixed menu'>
+            <div className='ui container center'>
+                <h2>Fake Shop</h2>
+            </div>
+        </header>
+    )
+}
+
