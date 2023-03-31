2bf3c3815652dde6c5a9dcf6e28ed7adbc44833e
cristianmeelo <cristianmeelo@gmail.com>
2023-03-31 16:49:02 -0300

    :truck: chore: import changed because moved redux files

--- react-redux-ecommerce/redux/store.ts
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from "redux"
-import logger from "redux-logger"
-import { reducers } from './reducers/index';
-
-export const store = createStore(reducers, {}, applyMiddleware(logger));
-
--- react-redux-ecommerce/src/main.tsx
+++ react-redux-ecommerce/src/main.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { Provider } from 'react-redux';
-import { store } from '../redux/store';
-
-
-
-
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
