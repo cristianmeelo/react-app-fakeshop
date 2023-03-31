e102670c2f0d172d53c31407b87c455294704032
cristianmeelo <cristianmeelo@gmail.com>
2023-03-31 16:45:38 -0300

    :recycle: refactor: refactored createStore

---
+++ react-redux-ecommerce/src/redux/store.ts
@@ -0,0 +1,6 @@
+import { legacy_createStore as createStore,applyMiddleware} from 'redux';
+import logger from "redux-logger"
+import { reducers } from './reducers/index';
+
+export const store = createStore(reducers, {}, applyMiddleware(logger));
+
