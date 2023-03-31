520664c6859444377f5fb7a8c90a7d3f379fd128
cristianmeelo <cristianmeelo@gmail.com>
2023-03-31 14:56:00 -0300

    :sparkles: feat: implemented routes in App

--- react-redux-ecommerce/src/App.tsx
+++ react-redux-ecommerce/src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Header } from "./components/header/Index"
+import { ProductListing } from './components/product/list/Index';
+import { ProductDetails } from './components/product/details/Index';
+
+
 export const App = () => {
   return (
-    <h1>App</h1>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" Component={ProductListing} />
+          <Route path="/product/:productId" Component={ProductListing} />
+          <Route>404 Not Foound</Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
