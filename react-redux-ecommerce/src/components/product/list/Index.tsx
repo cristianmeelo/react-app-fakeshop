0f648bd115e24cfe92b96f868ae02a7fd854dd41
cristianmeelo <cristianmeelo@gmail.com>
2023-03-31 14:55:44 -0300

    ⚛️ : feat: created productList component

---
+++ react-redux-ecommerce/src/components/product/list/Index.tsx
@@ -0,0 +1,11 @@
+
+import { ProductComponent } from "../component/Index";
+
+export const ProductListing = () => {
+
+    return (
+        <div className="ui grid container">
+            <ProductComponent />
+        </div>
+    )
+}
